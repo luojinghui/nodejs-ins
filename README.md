ff5f1ea1ee8cabbf92329b39141dd110a17e6433
luojinghui <939035243@qq.com>
2017-03-27 11:06:50 +0800

    init nodejs reptile

---
+++ README.md
@@ -0,0 +1,8 @@
+# Node.js-instagram
+Node.js批量抓取ins上的图片
+## 安装Node.js
+http://nodejs.org/download/
+## 安装依赖包
+npm install
+## 运行程序
+node app.js
---
+++ app.js
@@ -0,0 +1,60 @@
+/**
+ * Created by: Luojinghui/luojinghui424@gmail.com
+ * Date: 2017/2/7
+ * Time: 上午10:53
+ */
+
+//依赖模块
+var fs = require('fs');
+var request = require("request");
+var cheerio = require("cheerio");
+var mkdirp = require('mkdirp');
+
+//目标网址
+var baseUrl = 'https://api.instagram.com/v1/users/3421301663/media/recent/?access_token=3421301663.bf46c23.080fa8877b604b61931920f8820b1ced';
+
+//本地存储目录
+var dir = '../../source/img/photo';
+
+//创建目录
+mkdirp(dir, function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
+
+//发送请求
+request({uri: baseUrl}, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    let resData = JSON.parse(body).data;
+
+    resData.forEach(function (value, index) {
+      let imgSrc = value.images.standard_resolution.url;
+
+      console.log('正在下载原图' + imgSrc);
+      download(imgSrc, dir, value.id);
+      console.log('下载完成');
+    });
+
+    resData.forEach(function (value, index) {
+      let thumbnailSrc = value.images.thumbnail.url;
+
+      console.log('正在下载压缩图' + thumbnailSrc);
+      download(thumbnailSrc, dir, value.id + '.min');
+      console.log('下载完成');
+    });
+
+    //获取的json数据保存到本地备用
+    fs.writeFile('../../source/instagram/ins.json',body,function(err){
+      if(err) throw err;
+      console.log('write JSON into TEXT');
+    });
+  }
+});
+
+//下载方法
+var download = function (url, dir, filename) {
+  request.head(url, function (err, res, body) {
+    request(url).pipe(fs.createWriteStream(dir + "/" + filename + ".jpg"));
+  });
+};
